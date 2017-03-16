73abca4063c19ea026bd5a37facfc7d74dae3219
Michel Oleynik <michelole@gmail.com>
2017-03-16 16:39:43 +0100

    Maven project creation
    Creates default project using Eclipse template and add relevant
    .gitignore entries.

---
+++ src/main/java/at/medunigraz/imi/reassess/App.java
@@ -0,0 +1,13 @@
+package at.medunigraz.imi.reassess;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
