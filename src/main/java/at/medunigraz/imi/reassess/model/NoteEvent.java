0725ed8cce08645637242cb3c95cdc5c37356620
Michel Oleynik <michelole@gmail.com>
2017-03-29 20:45:07 +0200

    Maps columns and test them for nullity

--- src/main/java/at/medunigraz/imi/reassess/model/NoteEvent.java
+++ src/main/java/at/medunigraz/imi/reassess/model/NoteEvent.java
@@ -1,5 +1,7 @@
 package at.medunigraz.imi.reassess.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -15,7 +17,82 @@ public class NoteEvent {
 	@Column(name = "subject_id")
 	private Integer subjectId;
 
+	@Column(name = "hadm_id")
+	private Integer hospitalAdmissionId;
+
+	@Column(name = "chartdate")
+	private Date chartDate;
+
+	@Column(name = "charttime")
+	private Date chartTime;
+
+	@Column(name = "storetime")
+	private Date storeTime;
+
+	@Column(name = "category")
+	private String category;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "cgid")
+	private Integer caregiverId;
+
+	@Column(name = "iserror")
+	private Character isError;
+
+	@Column(name = "text")
+	private String text;
+
+	public Integer getRowId() {
+		return rowId;
+	}
+
 	public Integer getSubjectId() {
 		return subjectId;
+	}
+
+	public Integer getHospitalAdmissionId() {
+		return hospitalAdmissionId;
+	}
+
+	public Date getChartDate() {
+		return chartDate;
+	}
+
+	public Date getChartTime() {
+		return chartTime;
+	}
+
+	public Date getStoreTime() {
+		return storeTime;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Integer getCaregiverId() {
+		return caregiverId;
+	}
+
+	public boolean isError() {
+		return isError != null ? true : false;
+	}
+
+	public String getText() {
+		return text;
+	}
+	
+	@Override
+	public String toString() {
+		return "NoteEvent [rowId=" + rowId + ", subjectId=" + subjectId + ", hospitalAdmissionId=" + hospitalAdmissionId
+				+ ", chartDate=" + chartDate + ", chartTime=" + chartTime + ", storeTime=" + storeTime + ", category="
+				+ category + ", description=" + description + ", caregiverId=" + caregiverId + ", isError=" + isError
+				+ ", text=" + text.substring(0, 51) + "(...) ]";
 	}
 }
--- src/test/java/at/medunigraz/imi/reassess/dao/NoteEventDAOTest.java
+++ src/test/java/at/medunigraz/imi/reassess/dao/NoteEventDAOTest.java
@@ -8,7 +8,16 @@ public class NoteEventDAOTest extends TestCase {
 	public void testGet() {
 		NoteEventDAO noteDao = new NoteEventDAO();
 		NoteEvent note = noteDao.get(1);
+		
+		System.out.println(note);
+
 		assertNotNull(note.getSubjectId());
+		assertNotNull(note.getHospitalAdmissionId());
+		assertNotNull(note.getChartDate());
+		assertNotNull(note.getCategory());
+		assertNotNull(note.getDescription());
+		assertFalse(note.isError());
+		assertNotNull(note.getText());
 	}
 
 }
